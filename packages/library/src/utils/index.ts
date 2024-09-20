9ad6c173992fad002e41474d35f04d71396a0364
Rasik <vlad.fokin2004.vf@gmail.com>
2024-09-20 12:40:33 +0300

    Add a check for the presence of environment variables

--- packages/library/src/utils/index.ts
+++ packages/library/src/utils/index.ts
@@ -1,2 +1,8 @@
 export * from "./createSignature";
 export * from "./createForm";
+
+const { DOMAIN: domain, MERCHANT_LOGIN: merchantLogin, CURRENCY: currency, MERCHANT_SECRET_KEY: merchantSecret } = process.env;
+
+if (!domain || !merchantSecret || !merchantLogin || !currency) {
+    console.error('One of the environment variables required for wayforpay-ts-integration is not specified.')
+}
